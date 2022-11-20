63f46be05da4059dad01dda52d428e0401619983
Mueez Nadeem <mueeznadeem@gmail.com>
2022-11-20 22:29:10 +0500

    Database Linking + Additional screens
    20-11-2022
    
    Added linking to database & Additional Screens

--- app/src/main/java/com/example/sda_project/SignInPage.java
+++ app/src/main/java/com/example/sda_project/SignInPage.java
@@ -2,21 +2,40 @@ package com.example.sda_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class SignInPage extends AppCompatActivity {
-
+    EditText t1;
+    EditText t2;
+    DatabaseHelper myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        myDB= new DatabaseHelper(SignInPage.this);
+        try {
+            myDB.createDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        myDB.openDataBase();
         setContentView(R.layout.signinpage);
-    }
+        t1=(EditText) findViewById(R.id.UName);
+        t2=(EditText) findViewById(R.id.PWord);
+        }
+
 
     public void sendmsg(View view){
-        Intent i=new Intent(getApplicationContext(), RegisterPage.class);
-        startActivity(i);
-
+        boolean ans=myDB.CheckUserName(t1.getText().toString(),t2.getText().toString());
+       TextView a=(TextView) findViewById(R.id.Results);
+       String abc=new Boolean(ans).toString();
+       a.setText(abc);
     }
+    
+
 }
